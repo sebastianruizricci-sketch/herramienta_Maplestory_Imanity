a4f5087593518f5010177f16b899e72d76b7ed29
jhon-devu <jhon201144@gmail.com>
2026-06-10 13:11:31 -0500

    feat: add trades board CRUD

--- scripts/runtime-config.js
+++ scripts/runtime-config.js
@@ -1,7 +1,5 @@
 (function () {
   const renderApiBase = "https://herramienta-maplestory-imanity-api.onrender.com";
-  const hostname = window.location.hostname;
-  const isFirebaseHosting = hostname.endsWith(".web.app") || hostname.endsWith(".firebaseapp.com");
 
-  window.MAPLETOOLS_API_BASE = window.MAPLETOOLS_API_BASE || (isFirebaseHosting ? renderApiBase : "");
+  window.MAPLETOOLS_API_BASE = window.MAPLETOOLS_API_BASE || renderApiBase;
 })();
